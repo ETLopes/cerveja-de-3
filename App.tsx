3360562d02f5a43148d4232de50c24e56bac2635
ETLopes <eduardotorreslopes@gmail.com>
2020-05-07 18:42:13 -0300

    feat: implement push notifications

--- App.tsx
+++ App.tsx
@@ -2,12 +2,41 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import OneSignal from 'react-native-onesignal';
+
 import Home from './src/screens/Home';
 import {Text} from 'react-native';
 
 const Stack = createStackNavigator();
 
-function App() {
+function App(this: any) {
+  OneSignal.init('8fea3775-343f-4378-a271-8b14da5f5b94', {
+    kOSSettingsKeyAutoPrompt: false,
+    kOSSettingsKeyInAppLaunchURL: false,
+    kOSSettingsKeyInFocusDisplayOption: 2,
+  });
+
+  const onReceived = (notification) => {
+    console.log('Notification received: ', notification);
+  };
+
+  const onOpened = (openResult) => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  };
+
+  const onIds = (device) => {
+    console.log('Device info: ', device);
+  };
+
+  OneSignal.setLogLevel(6, 0);
+
+  OneSignal.addEventListener('received', onReceived);
+  OneSignal.addEventListener('opened', onOpened);
+  OneSignal.addEventListener('ids', onIds);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
